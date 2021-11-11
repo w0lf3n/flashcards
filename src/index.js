4b405ae9ba0211f74ce9764263823ea89e5042cf
w0lf3n <w0lf3n@coole-files.de>
2021-11-11 20:07:17 +0100

    new start features

--- src/index.js
+++ src/index.js
@@ -19,6 +19,7 @@ const vocabulary_list = [
 ];
 
 let flag_reveal_immediately = false;
+let flag_direction_eng_ger = true;
 let flag_restart = true;
 let state_quest = true;
 
@@ -43,8 +44,13 @@ const show_next_card = function () {
 
         const vocable = vocabulary_list[vocabulary_index].split(";");
 
-        question.textContent = vocable[0];
-        solution.textContent = vocable[1];
+        if (flag_direction_eng_ger) {
+            question.textContent = vocable[0];
+            solution.textContent = vocable[1];
+        } else {
+            question.textContent = vocable[1];
+            solution.textContent = vocable[0];
+        }
 
         vocabulary_index = vocabulary_index + 1;
 
@@ -54,6 +60,7 @@ const show_next_card = function () {
         solution.style.display = "none";
         document.body.appendChild(reveal_box);
 
+        flag_restart = true;
     }
 
 };
@@ -114,15 +121,32 @@ const create_elements = function () {
 
 };
 
-const start_tester = function (reveal_immediately) {
-    flag_reveal_immediately = reveal_immediately;
+const start_tester = function () {
     prompt_box.style.display = "none";
-    flag_restart = true;
     state_quest = true;
     vocabulary_index = 0;
     shuffle(vocabulary_list);
     create_elements();
     show_next_card();
+};
+
+const createRadioButton = function (groupName, buttonId, value, action, checked) {
+    const container = document.createElement("div");
+    const radiobutton = document.createElement("input");
+    container.appendChild(radiobutton);
+    radiobutton.id = buttonId;
+    radiobutton.addEventListener("change", action);
+    radiobutton.setAttribute("name", groupName);
+    radiobutton.setAttribute("type", "radio");
+    if (checked) {
+        radiobutton.setAttribute("checked", "checked");
+    }
+    const label = document.createElement("label");
+    label.setAttribute("for", buttonId);
+    label.textContent = value;
+    container.appendChild(label);
+
+    return container;
 };
 
 const create_prompt = function () {
@@ -131,19 +155,41 @@ const create_prompt = function () {
     prompt_box.className = "PromptBox";
     document.body.appendChild(prompt_box);
 
-    const button_reveal_at_end = document.createElement("button");
-    button_reveal_at_end.textContent = "reveal at the end";
-    button_reveal_at_end.addEventListener("click", () => {
-        start_tester(false);
-    });
-    prompt_box.appendChild(button_reveal_at_end);
+    const fieldset_language = document.createElement("fieldset");
+    prompt_box.appendChild(fieldset_language);
+    const legend_language = document.createElement("legend");
+    legend_language.textContent = "language direction";
+    fieldset_language.appendChild(legend_language);
+    const button_english = createRadioButton("language", "english", "english -> german", () => {
+        flag_direction_eng_ger = true;
+    }, true);
+    fieldset_language.appendChild(button_english);
+    const button_german = createRadioButton("language", "german", "german -> english", () => {
+        console.log("clicked ger -> eng");
+        flag_direction_eng_ger = false;
+    }, false);
+    fieldset_language.appendChild(button_german);
 
-    const button_reveal_immediately = document.createElement("button");
-    button_reveal_immediately.textContent = "reveal immediately";
-    button_reveal_immediately.addEventListener("click", () => {
-        start_tester(true);
+    const fieldset_reveal = document.createElement("fieldset");
+    prompt_box.appendChild(fieldset_reveal);
+    const legend_reveal = document.createElement("legend");
+    legend_reveal.textContent = "reveal answer";
+    fieldset_reveal.appendChild(legend_reveal);
+    const button_reveal_at_the_end = createRadioButton("reveal", "at_the_end", "at the end", () => {
+        flag_reveal_immediately = false;
+    }, true);
+    fieldset_reveal.appendChild(button_reveal_at_the_end);
+    const button_reveal_immediately = createRadioButton("reveal", "immediately", "immediately", () => {
+        flag_reveal_immediately = true;
+    }, false);
+    fieldset_reveal.appendChild(button_reveal_immediately);
+
+    const button_start = document.createElement("button");
+    button_start.textContent = "start";
+    button_start.addEventListener("click", () => {
+        start_tester();
     });
-    prompt_box.appendChild(button_reveal_immediately);
+    prompt_box.appendChild(button_start);
 
 };
 
